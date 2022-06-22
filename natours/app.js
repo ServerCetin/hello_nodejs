89fc1c0ab0e1621a970dfa21a84ff7d97cc9ebfa
Server ÇETİN <servercetin@hotmail.com>
2022-06-22 03:48:37 +0300

    env variables

--- natours/app.js
+++ natours/app.js
@@ -8,9 +8,12 @@ const userRouter = require('./routes/userRoutes')
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+if(process.env.NODE_ENV==="development"){
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`)) // access to static files http://localhost:3000/overview.html
 
 app.use((req,res,next)=>{
   console.log('Middleware here!');
